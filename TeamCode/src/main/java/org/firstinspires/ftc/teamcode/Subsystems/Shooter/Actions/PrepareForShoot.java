8229281a2ec31c5270cb1781c6d9ccb8ca578410
André <151085951+DARR-1@users.noreply.github.com>
2025-11-19 23:10:34 -0600

    Update PrepareForShoot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter/Actions/PrepareForShoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter/Actions/PrepareForShoot.java
@@ -18,7 +18,7 @@ public class PrepareForShoot implements Action {
     private final Supplier<Double> botYaw;
 
     private final double targetHeight = 0.7;
-    private double accel = -9.81;
+    private double accel = 9.81;
     private final Telemetry telemetry;
 
     private double pitch;
